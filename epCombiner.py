040512089df8ea48caa584fad7d04c243a96bb8b
Shelly Sicat <shellygsicat@gmail.com>
2020-01-27 22:09:23 -0700

    modularize code

--- epCombiner.py
+++ epCombiner.py
@@ -1,10 +1,14 @@
+#---------------------------------
+# Inputs
+#---------------------------------
 import subprocess
 import os
 from shutil import move
 from os.path import *
 
-
-#File Directory 
+#---------------------------------
+# File Directory
+#---------------------------------
 fileDir = os.path.dirname(os.path.abspath(__file__))    # Directory of the Module
 filePath = fileDir + "\\" 
 combinerFile = 'combiner.bat'
@@ -13,43 +17,48 @@ combinerFilePath = fileDir + "\\" + combinerFile
 exeFilePath = fileDir + "\\" + exeFile
 audioFolderPath = fileDir + "\\audio\\"
 
-
+#---------------------------------
+# Variables 
+#---------------------------------
 showName = "anohana"
-
 audioFiles = os.listdir(audioFolderPath)
+currentEpisode = 1 
 
 # print (audioFiles)
 
-currentEpisode = 1
+#---------------------------------
+# Functions
+#---------------------------------
+def get_currentEpisodeString(currentEpisode):
+    currentEpisodeString = str(currentEpisode).rjust(2, "0") if currentEpisode < 10 else str(currentEpisode)
+    return currentEpisodeString
 
+def combineAndComplete():
+    completedEpisodeFilePath = filePath+showName+currentEpisodeString +".mp3"       
+    # combine mp3 
+    subprocess.call(combinerFilePath)
+    # rename completed file 
+    os.rename(filePath+'combined.mp3',completedEpisodeFilePath)
+    # move to a completed folder 
+    move(completedEpisodeFilePath, fileDir+"\\completed\\")
+
+#---------------------------------
+# Loop Through
+#---------------------------------
 for audioFile in audioFiles:
-    currentEpisodeString = str(currentEpisode).rjust(2, "0") if currentEpisode < 10 else str(currentEpisode)    
+    currentEpisodeString = get_currentEpisodeString(1)    
     if showName+"_"+currentEpisodeString in audioFile :
         print('found')
         # move to main folder
         move(audioFolderPath+audioFile, filePath)  
     else:
-        completedEpisodeFilePath = filePath+showName+currentEpisodeString +".mp3"       
-        # combine mp3 
-        subprocess.call(combinerFilePath)
-        # rename completed file 
-        os.rename(filePath+'combined.mp3',completedEpisodeFilePath)
-        # move to a completed folder 
-        move(completedEpisodeFilePath, fileDir+"\\completed\\")
+        combineAndComplete()
         # increment counter
-        currentEpisode = currentEpisode+1
+        currentEpisode += 1
         print(currentEpisode)
         # move current audiofile to main folder
         move(audioFolderPath+audioFile, filePath) 
 
 # final run
-
-currentEpisodeString = str(currentEpisode).rjust(2, "0") if currentEpisode < 10 else str(currentEpisode)    
-
-completedEpisodeFilePath = filePath+showName+currentEpisodeString +".mp3"       
-# combine mp3 
-subprocess.call(combinerFilePath)
-# rename completed file 
-os.rename(filePath+'combined.mp3',completedEpisodeFilePath)
-# move to a completed folder 
-move(completedEpisodeFilePath, fileDir+"\\completed\\")
+currentEpisodeString = get_currentEpisodeString(1)  
+combineAndComplete()
